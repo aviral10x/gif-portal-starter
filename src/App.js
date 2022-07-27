85e07382b3e0a135f580bb0814f44970d0a72cca
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-07-28 02:39:19 +0530

    gif

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import "./App.css";
 const TWITTER_HANDLE = "aviral10x";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
+
+const TEST_GIFS = [
+  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
+  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
+  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
+  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+];
+
 const App = () => {
 
   const [walletAddress, setWalletAddress] = useState(null);
@@ -47,7 +55,19 @@ const checkIfWalletIsConnected = async () => {
      >
        Connect to Wallet
      </button>
-   );
+  );
+  
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <div className="gif-grid">
+        {TEST_GIFS.map((gif) => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 
   useEffect(() => {
     const onLoad = async () => {
@@ -65,7 +85,9 @@ const checkIfWalletIsConnected = async () => {
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
-          {renderNotConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
+
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />

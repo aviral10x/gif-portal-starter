6e3fabd8bb179b3de8beb82da80237abd0ce649e
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-07-28 02:31:52 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
 
@@ -6,6 +6,9 @@ const TWITTER_HANDLE = "aviral10x";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
 const checkIfWalletIsConnected = async () => {
   try {
     const { solana } = window;
@@ -25,7 +28,26 @@ const checkIfWalletIsConnected = async () => {
   } catch (error) {
     console.error(error);
   }
-};
+  };
+  
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+   const renderNotConnectedContainer = () => (
+     <button
+       className="cta-button connect-wallet-button"
+       onClick={connectWallet}
+     >
+       Connect to Wallet
+     </button>
+   );
 
   useEffect(() => {
     const onLoad = async () => {
@@ -34,14 +56,16 @@ const checkIfWalletIsConnected = async () => {
     window.addEventListener("load", onLoad);
     return () => window.removeEventListener("load", onLoad);
   }, []);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
